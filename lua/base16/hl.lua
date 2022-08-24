2e78b41abd5d24ddd79c56aada01d8837e9bd4bf
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-24 15:31:07 +0200

    Base16

---
+++ lua/base16/hl.lua
@@ -0,0 +1,28 @@
+local merge_tb = require("base16").merge_tb
+
+local highlights = {}
+local hl_dir = vim.fn.stdpath "data" .. "/site/pack/packer/start/base16/lua/base16/highlight"
+
+-- push hl_dir file names to table
+local hl_files = require("plenary.scandir").scan_dir(hl_dir, {})
+
+for _, file in ipairs(hl_files) do
+  local a = vim.fn.fnamemodify(file, ":t")
+  a = vim.fn.fnamemodify(a, ":r")
+
+  local integration = require("base16.highlight." .. a)
+  highlights = merge_tb(highlights, integration)
+end
+
+-- polish theme specific highlights
+local polish_hl = require("base16").get_theme_tb "polish_hl"
+
+if polish_hl then
+  highlights = merge_tb(highlights, polish_hl)
+end
+
+if vim.g.transparency then
+  highlights = merge_tb(highlights, require "base16.glassy")
+end
+
+return highlights
