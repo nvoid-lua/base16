77c54301fd1476b04d65030c11e9927ac600a10e
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-05 20:19:58 +0300

    update

--- lua/base16/init.lua
+++ lua/base16/init.lua
@@ -1,171 +1,186 @@
 local M = {}
 local g = vim.g
 local fn = vim.fn
+local base16_path = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":p:h")
 
 M.get_theme_tb = function(type)
-  local default_path = "base16.themes." .. g.theme
+	local default_path = "base16.themes." .. g.theme
+	local user_path = "custom.themes." .. g.theme
 
-  local present1, default_theme = pcall(require, default_path)
+	local present1, default_theme = pcall(require, default_path)
+	local present2, user_theme = pcall(require, user_path)
 
-  if present1 then
-    return default_theme[type]
-  else
-    error "No such theme bruh >_< "
-  end
+	if present1 then
+		return default_theme[type]
+	elseif present2 then
+		return user_theme[type]
+	else
+		error("No such theme!")
+	end
 end
 
-M.merge_tb = function(table1, table2)
-  return vim.tbl_deep_extend("force", table1, table2)
-end
-
---  credits to https://github.com/max397574 for this function
-M.clear_highlights = function(hl_group)
-  local highlights_raw = vim.split(vim.api.nvim_exec("filter " .. hl_group .. " hi", true), "\n")
-  local highlight_groups = {}
-
-  for _, raw_hi in ipairs(highlights_raw) do
-    table.insert(highlight_groups, string.match(raw_hi, hl_group .. "%a+"))
-  end
-
-  for _, highlight in ipairs(highlight_groups) do
-    vim.cmd([[hi clear ]] .. highlight)
-  end
-end
-
-M.load_all_highlights = function()
-  vim.opt.bg = require("base16").get_theme_tb "type" -- dark/light
-
-  -- reload highlights for theme switcher
-  local reload = require("plenary.reload").reload_module
-  local clear_hl = require("base16").clear_highlights
-
-  clear_hl "BufferLine"
-  clear_hl "TS"
-
-  reload "base16.integrations"
-  reload "base16.hl"
-
-  local hl_groups = require "base16.hl"
-
-  for hl, col in pairs(hl_groups) do
-    vim.api.nvim_set_hl(0, hl, col)
-  end
+M.merge_tb = function(...)
+	return vim.tbl_deep_extend("force", ...)
 end
 
 M.turn_str_to_color = function(tb)
-  local colors = M.get_theme_tb "base_30"
+	local colors = M.get_theme_tb("base_30")
+	local copy = vim.deepcopy(tb)
 
-  for _, groups in pairs(tb) do
-    for k, v in pairs(groups) do
-      if k == "fg" or k == "bg" then
-        if v:sub(1, 1) == "#" or v == "none" or v == "NONE" then
-        else
-          groups[k] = colors[v]
-        end
-      end
-    end
-  end
+	for _, hlgroups in pairs(copy) do
+		for opt, val in pairs(hlgroups) do
+			if
+				(opt == "fg" or opt == "bg" or opt == "sp")
+				and not (val:sub(1, 1) == "#" or val == "none" or val == "NONE")
+			then
+				hlgroups[opt] = colors[val]
+			end
+		end
+	end
 
-  return tb
+	return copy
 end
 
 M.extend_default_hl = function(highlights)
-  local glassy = require "base16.glassy"
-  local polish_hl = M.get_theme_tb "polish_hl"
+	local polish_hl = M.get_theme_tb("polish_hl")
 
-  -- polish themes
-  if polish_hl then
-    for key, value in pairs(polish_hl) do
-      if highlights[key] then
-        highlights[key] = M.merge_tb(highlights[key], value)
-      end
-    end
-  end
+	-- polish themes
+	if polish_hl then
+		for key, value in pairs(polish_hl) do
+			if highlights[key] then
+				highlights[key] = M.merge_tb(highlights[key], value)
+			end
+		end
+	end
 
-  -- transparency
-  if vim.g.transparency then
-    for key, value in pairs(glassy) do
-      if highlights[key] then
-        highlights[key] = M.merge_tb(highlights[key], value)
-      end
-    end
-  end
+	-- transparency
+	if vim.g.transparency then
+		local glassy = require("base16.glassy")
 
-  local overriden_hl = M.turn_str_to_color(nvoid.ui.hl_override)
+		for key, value in pairs(glassy) do
+			if highlights[key] then
+				highlights[key] = M.merge_tb(highlights[key], value)
+			end
+		end
+	end
 
-  for key, value in pairs(overriden_hl) do
-    if highlights[key] then
-      highlights[key] = M.merge_tb(highlights[key], value)
-    end
-  end
+	if nvoid.ui.hl_override then
+		local overriden_hl = M.turn_str_to_color(nvoid.ui.hl_override)
+
+		for key, value in pairs(overriden_hl) do
+			if highlights[key] then
+				highlights[key] = M.merge_tb(highlights[key], value)
+			end
+		end
+	end
 end
 
 M.load_highlight = function(group)
-  if type(group) == "string" then
-    group = require("base16.integrations." .. group)
-    M.extend_default_hl(group)
-  end
-
-  for hl, col in pairs(group) do
-    vim.api.nvim_set_hl(0, hl, col)
-  end
+	group = require("base16.integrations." .. group)
+	M.extend_default_hl(group)
+	return group
 end
 
-M.load_theme = function()
-  M.load_highlight "defaults"
-  M.load_highlight "statusline"
-  M.load_highlight(M.turn_str_to_color(nvoid.ui.hl_add))
+-- convert table into string
+M.table_to_str = function(tb)
+	local result = ""
+
+	for hlgroupName, hlgroup_vals in pairs(tb) do
+		local hlname = "'" .. hlgroupName .. "',"
+		local opts = ""
+
+		for optName, optVal in pairs(hlgroup_vals) do
+			local valueInStr = ((type(optVal)) == "boolean" or type(optVal) == "number") and tostring(optVal)
+				or '"' .. optVal .. '"'
+			opts = opts .. optName .. "=" .. valueInStr .. ","
+		end
+
+		result = result .. "vim.api.nvim_set_hl(0," .. hlname .. "{" .. opts .. "})"
+	end
+
+	return result
 end
 
-M.toggle_transparency = function()
-  local transparency_status = nvoid.ui.transparency
-  local write_data = require("nvchad").write_data
+M.saveStr_to_cache = function(filename, tb)
+	-- Thanks to https://github.com/nullchilly and https://github.com/EdenEast/nightfox.nvim
+	-- It helped me understand string.dump stuff
 
-  local function save_chadrc_data()
-    local old_data = "transparency = " .. tostring(transparency_status)
-    local new_data = "transparency = " .. tostring(g.transparency)
+	local bg_opt = "vim.opt.bg='" .. M.get_theme_tb("type") .. "'"
+	local defaults_cond = filename == "defaults" and bg_opt or ""
 
-    write_data(old_data, new_data)
-  end
+	local lines = "return string.dump(function()" .. defaults_cond .. M.table_to_str(tb) .. "end, true)"
+	local file = io.open(vim.g.base16_cache .. filename, "wb")
 
-  if g.transparency then
-    g.transparency = false
-    M.load_all_highlights()
-    save_chadrc_data()
-  else
-    g.transparency = true
-    M.load_all_highlights()
-    save_chadrc_data()
-  end
+	if file then
+		file:write(loadstring(lines)())
+		file:close()
+	end
 end
 
-M.reload_theme = require "base16.reload_theme"
+M.compile = function()
+	if not vim.loop.fs_stat(vim.g.base16_cache) then
+		vim.fn.mkdir(vim.g.base16_cache, "p")
+	end
+
+	-- All integration modules, each file returns a table
+	local hl_files = base16_path .. "/integrations"
+
+	for _, file in ipairs(vim.fn.readdir(hl_files)) do
+		-- skip caching some files
+		if file ~= "statusline" or file ~= "treesitter" then
+			local filename = vim.fn.fnamemodify(file, ":r")
+			M.saveStr_to_cache(filename, M.load_highlight(filename))
+		end
+	end
+
+	-- look for custom cached highlight files
+	local extended_integrations = nvoid.ui.extended_integrations
+
+	if extended_integrations then
+		for _, integration in ipairs(extended_integrations) do
+			M.saveStr_to_cache(integration, require("base16.extended_integrations." .. integration))
+		end
+	end
+end
+
+M.load_all_highlights = function()
+	require("plenary.reload").reload_module("base16")
+	M.compile()
+
+	for _, file in ipairs(vim.fn.readdir(vim.g.base16_cache)) do
+		dofile(vim.g.base16_cache .. file)
+	end
+end
 
 M.list_themes = function()
-  local default_themes = vim.fn.readdir(vim.fn.stdpath "data" .. "/site/pack/lazy/opt/base16/lua/base16/themes")
-  default_themes = default_themes
-  for index, theme in ipairs(default_themes) do
-    default_themes[index] = fn.fnamemodify(fn.fnamemodify(theme, ":t"), ":r")
-  end
+	local default_themes = vim.fn.readdir(vim.fn.stdpath("data") .. "/site/pack/lazy/opt/base16/lua/base16/themes")
+	-- local default_themes = "base16.themes." .. g.theme
 
-  return default_themes
-end
+	local custom_themes = vim.loop.fs_stat(fn.stdpath("config") .. "/lua/custom/themes")
 
-M.clear_cmdline = function()
-  vim.defer_fn(function()
-    vim.cmd "echo"
-  end, 0)
+	if custom_themes and custom_themes.type == "directory" then
+		local themes_tb = fn.readdir(fn.stdpath("config") .. "/lua/custom/themes")
+		for _, value in ipairs(themes_tb) do
+			default_themes[#default_themes + 1] = value
+		end
+	end
+
+	for index, theme in ipairs(default_themes) do
+		default_themes[index] = theme:match("(.+)%..+")
+	end
+
+	return default_themes
 end
 
 M.replace_word = function(old, new)
-  local chadrc = vim.fn.stdpath "config" .. "/config.lua"
-  local file = io.open(chadrc, "r")
-  local added_pattern = string.gsub(old, "-", "%%-") -- add % before - if exists
-  local new_content = file:read("*all"):gsub(added_pattern, new)
+	local chadrc = vim.fn.stdpath("config") .. "/lua/custom/" .. "chadrc.lua"
+	local file = io.open(chadrc, "r")
+	local added_pattern = string.gsub(old, "-", "%%-") -- add % before - if exists
+	local new_content = file:read("*all"):gsub(added_pattern, new)
 
-  file = io.open(chadrc, "w")
-  file:write(new_content)
-  file:close()
+	file = io.open(chadrc, "w")
+	file:write(new_content)
+	file:close()
 end
 
 return M
--- lua/telescope/_extensions/themes.lua
+++ lua/telescope/_extensions/themes.lua
@@ -1,111 +1,81 @@
-local function theme_switcher(opts)
-  local pickers, finders, previewers, actions, action_state, utils, conf
-  if pcall(require, "telescope") then
-    pickers = require "telescope.pickers"
-    finders = require "telescope.finders"
-    previewers = require "telescope.previewers"
-    actions = require "telescope.actions"
-    action_state = require "telescope.actions.state"
-    utils = require "telescope.utils"
-    conf = require("telescope.config").values
-  else
-    error "Cannot find telescope!"
-  end
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local previewers = require("telescope.previewers")
 
-  local local_utils = require "base16"
-  local reload_theme = local_utils.reload_theme
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
-  -- get a table of available themes
-  local themes = local_utils.list_themes()
-  if next(themes) ~= nil then
-    -- save this to use it for later to restore if theme not changed
-    local current_theme = vim.g.theme
-    local new_theme = ""
-    local change = false
-
-    -- buffer number and name
-    local bufnr = vim.api.nvim_get_current_buf()
-    local bufname = vim.api.nvim_buf_get_name(bufnr)
-
-    local previewer
-
-    -- in case its not a normal buffer
-    if vim.fn.buflisted(bufnr) ~= 1 then
-      local deleted = false
-      local function del_win(win_id)
-        if win_id and vim.api.nvim_win_is_valid(win_id) then
-          utils.buf_delete(vim.api.nvim_win_get_buf(win_id))
-          pcall(vim.api.nvim_win_close, win_id, true)
-        end
-      end
-
-      previewer = previewers.new {
-        preview_fn = function(_, entry, status)
-          if not deleted then
-            deleted = true
-            del_win(status.preview_win)
-            del_win(status.preview_border_win)
-          end
-          reload_theme(entry.value)
-        end,
-      }
-    else
-      -- show current buffer content in previewer
-      previewer = previewers.new_buffer_previewer {
-        define_preview = function(self, entry)
-          local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
-          local filetype = require("plenary.filetype").detect(bufname) or "diff"
-
-          require("telescope.previewers.utils").highlighter(self.state.bufnr, filetype)
-          reload_theme(entry.value)
-        end,
-      }
-    end
-
-    local picker = pickers.new {
-      prompt_title = "Set Theme",
-      finder = finders.new_table(themes),
-      previewer = previewer,
-      sorter = conf.generic_sorter(opts),
-      attach_mappings = function(prompt_bufnr)
-        actions.select_default:replace(function()
-          if action_state.get_selected_entry() then
-            actions.close(prompt_bufnr)
-            require("base16").replace_word(current_theme, action_state.get_selected_entry()[1])
-          end
-        end)
-        return true
-      end,
-    }
-
-    -- rewrite picker.close_windows
-    local close_windows = picker.close_windows
-    picker.close_windows = function(status)
-      -- now apply the theme, if success, then ask for default theme change
-      local final_theme
-      if change then
-        final_theme = new_theme
-      else
-        final_theme = current_theme
-      end
-      vim.g.theme = final_theme
-      close_windows(status)
-    end
-    picker:find()
-  else
-    print("No themes found in " .. vim.fn.stdpath "config" .. "/lua/themes")
-  end
+local function reload_theme(name)
+	vim.g.theme = name
+	require("base16").load_all_highlights()
+	vim.api.nvim_exec_autocmds("User", { pattern = "ThemeReload" })
 end
 
--- register theme swticher as themes to telescope
-local present, telescope = pcall(require, "telescope")
-if present then
-  return telescope.register_extension {
-    exports = {
-      themes = theme_switcher,
-    },
-  }
-else
-  error "Cannot find telescope!"
+local function switcher()
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	-- show current buffer content in previewer
+	local previewer = previewers.new_buffer_previewer({
+		define_preview = function(self, entry)
+			-- add content
+			local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+			vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+
+			-- add syntax highlighting in previewer
+			local ft = (vim.filetype.match({ buf = bufnr }) or "diff"):match("%w+")
+			require("telescope.previewers.utils").highlighter(self.state.bufnr, ft)
+
+			reload_theme(entry.value)
+		end,
+	})
+
+	-- our picker function: colors
+	local picker = pickers.new({
+		prompt_title = "Set Theme",
+		previewer = previewer,
+		finder = finders.new_table({
+			results = require("base16").list_themes(),
+		}),
+		sorter = conf.generic_sorter(),
+
+		attach_mappings = function(prompt_bufnr, map)
+			-- reload theme while typing
+			vim.schedule(function()
+				vim.api.nvim_create_autocmd("TextChangedI", {
+					buffer = prompt_bufnr,
+					callback = function()
+						if action_state.get_selected_entry() then
+							reload_theme(action_state.get_selected_entry()[1])
+						end
+					end,
+				})
+			end)
+			-- reload theme on cycling
+			map("i", "<C-n>", function()
+				actions.move_selection_next(prompt_bufnr)
+				reload_theme(action_state.get_selected_entry()[1])
+			end)
+
+			map("i", "<C-p>", function()
+				actions.move_selection_previous(prompt_bufnr)
+				reload_theme(action_state.get_selected_entry()[1])
+			end)
+
+			------------ save theme to chadrc on enter ----------------
+			actions.select_default:replace(function()
+				if action_state.get_selected_entry() then
+					actions.close(prompt_bufnr)
+					require("base16").replace_word(nvoid.ui.colorscheme, action_state.get_selected_entry()[1])
+				end
+			end)
+			return true
+		end,
+	})
+
+	picker:find()
 end
+
+return require("telescope").register_extension({
+	exports = { themes = switcher },
+})
